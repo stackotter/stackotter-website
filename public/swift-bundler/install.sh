78d57c777244764d29a3cc833cb7f5117c532a38
stackotter <stackotter@stackotter.dev>
2022-04-03 08:26:56 +1000

    Update Swift Bundler installation script and installation instructions

--- public/swift-bundler/install.sh
+++ public/swift-bundler/install.sh
@@ -13,8 +13,23 @@ TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 BRANCH=${1:-$TAG}
 git -c advice.detachedHead=false checkout $BRANCH
 
-# Install swift-bundler
-./install.sh
+# Ensure that xcode command-line tools are installed
+echo "Ensuring that the Xcode CLTs are installed"
+xcode-select --install 2>/dev/null || true
+
+# Build
+echo "Building swift-bundler"
+swift build -c release
+
+# Create directory with correct permissions
+echo "Your password is required to copy the executable to /opt/swift-bundler"
+sudo mkdir -p -m755 /opt/swift-bundler
+
+# Copy executable
+sudo cp .build/release/swift-bundler /opt/swift-bundler
+
+# Add to PATH if it isn't in PATH already
+[[ ":$PATH:" != *":/opt/swift-bundler:"* ]] && printf "\nexport PATH=\"/opt/swift-bundler:\$PATH\"" >> ~/.zshenv && echo "\nRun \`source ~/.zshenv\` to finish installation"
 
 # Clean up
 echo "\nCleaning up"
@@ -22,3 +37,6 @@ cd ../..
 rm -rf $DIR
 
 echo "Done"
+
+# Exit with the success code (the path updating command makes the exit code a failure when /opt/swift-bundler is already in PATH)
+exit 0
